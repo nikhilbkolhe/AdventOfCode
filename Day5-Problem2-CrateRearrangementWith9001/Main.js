68561ffe899d000d7d452d9ec21031c8fec2e6f8
nikhilbkolhe <nikhilbkolhe@gmail.com>
2022-12-06 01:06:29 +0530

    Advent Of Code Day5 Problem 2 CrateMover9001

---
+++ Day5-Problem2-CrateRearrangementWith9001/Main.js
@@ -0,0 +1,41 @@
+import {open } from 'fs/promises'
+
+const file = await open ('./input.txt');
+
+//hardcoded the first part inputs lol
+// try to parse them as well
+const input = [
+'QFMRLWCV'
+,'DQL'
+,'PSRGWCNB'
+,'LCDHBQG'
+,'VGLFZS'
+,'DGNP'
+,'DZPVFCW'
+,'CPDMS'
+,'ZNWTVMPC'
+]
+
+// regex to extract 
+const regex = /^move (\d+) from (\d+) to (\d+)$/;
+
+
+const moveCrates = (number, from, to) => {
+    // simulate removing top n crates from source stack.
+    let removedCrates = input[from].substring(input[from].length - number);
+    // update source stack after removing  top n crates
+    input[from] = input[from].substring(0,input[from].length - number);
+    // Since CrateMove9001 can move multiple crates at once, their order is not reversed
+    // while placement on target stack
+    input[to] = input[to].concat(removedCrates);
+}
+
+
+for await (let instruction of file.readLines()){
+    let [number, from, to] = instruction.replace(regex,'$1-$2-$3').split('-');
+    moveCrates(parseInt(number),parseInt(from) - 1, parseInt(to) -1);
+}
+
+console.log(input.reduce((topCrates, stack) => {
+   return topCrates.concat(stack[stack.length - 1]);
+},''))
