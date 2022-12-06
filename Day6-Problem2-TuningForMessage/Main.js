6cf2d6db9568089f8019c1a005a8ac707980fc42
Nikhil Kolhe <37927139+nikhilbkolhe@users.noreply.github.com>
2022-12-07 01:47:03 +0530

    Update Main.js

--- Day6-Problem2-TuningForMessage/Main.js
+++ Day6-Problem2-TuningForMessage/Main.js
@@ -6,9 +6,9 @@ let inputSignal = fs.readFileSync('./input.txt').toString();
 function findStartOfPacket(ipstring){
     // for each character in string
     for (let i = 0; i < ipstring.length ; i++){
-        //take next three characters including current in a set
+        //take next thirteen characters and current in a set
         let set = new Set(ipstring.substring(i,i+14));
-        //if size of set is 4 means current 4 characters of string are all different
+        //if size of set is 14 means current 14 characters of string are all different
         // which is start of packet
         if (set.size === 14){
             return i+13+1;
